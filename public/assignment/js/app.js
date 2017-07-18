1c6aa35edc35555dc4568914e98c650ba998f549
Sharmodeep Sarkar <sharmodeepsarkar25@gmail.com>
2017-07-18 06:40:32 -0400

    till wizard-list.html

---
+++ public/assignment/js/app.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Sharmo on 7/17/2017.
+ */
+var app = angular.module("WamApp", ["ngRoute"]);
+
+app.controller("loginController", loginController);
+app.controller("profileController", profileController);
+
+// the configuration occurs at startup of the app
+app.config(configurationFunction);
+
+function configurationFunction($routeProvider){
+    $routeProvider
+        .when("/login", {
+            templateUrl: "login_partial.html"
+        }) // means, if you login in url .. render the corresponding html
+        .when("/register", {
+            templateUrl: "register.html"
+        })
+        .when("/profile/:userId", {
+            templateUrl: "profile_partial.html"
+        })
+}
+
+// $scope is the variable that binds the view and the controller
+function loginController($scope, $location) {
+    var valid_users =[
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    $scope.hello = "Hello from loginController";
+    $scope.login = function (user) {
+        // alert(user.username + "  :   "+user.password);
+        for (var u in valid_users){
+            var _user = valid_users[u];
+            if (_user.username === user.username && _user.password === user.password){
+                $scope.welcomeUser = _user;
+                $location.url("profile/"+_user._id)
+            }
+        }
+        // we have looped over all the valid users and not routed out,
+        //      hence wrong username or passwrord
+        if ($scope.welcomeUser === undefined ){
+            $scope.errorMessage = "Wrong Username or Password"
+        }
+    }
+}
+
+
+function profileController($scope, $routeParams) {
+    var user_id = $routeParams['userId']
+    alert(user_id);
+}
